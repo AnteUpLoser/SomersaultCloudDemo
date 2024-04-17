d9a361640ccade74943dd874e612238d0f06f5e8
AnteUpLoser <1750859115@qq.com>
2024-04-17 15:41:43 +0800

    test: 密码校验测试

--- src/test/java/com/minigptdemo/MiniGptDemoApplicationTests.java
+++ src/test/java/com/minigptdemo/MiniGptDemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.minigptdemo;
 
+import com.minigptdemo.utils.encryption.EncryptUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +8,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 class MiniGptDemoApplicationTests {
 
     @Test
-    void contextLoads() {
+    public static void main(String[] args) {
+        String password = "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92";
+        System.out.println(EncryptUtil.verify("123456", password));
     }
 
 }
