53b723b323342ecb3e3d5bf35497cb2f63db0cd4
AnteUpLoser <1750859115@qq.com>
2024-04-27 11:07:36 +0800

    refactor: 密码加盐

--- src/main/java/com/demo/utils/encryption/EncryptUtil.java
+++ src/main/java/com/demo/utils/encryption/EncryptUtil.java
@@ -3,8 +3,11 @@ package com.demo.utils.encryption;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 
 public class EncryptUtil {
+
+    private static final String salt = "SomersaultCloud";
 
     /**
      * 私有化实例
@@ -12,21 +15,17 @@ public class EncryptUtil {
     private EncryptUtil(){}
 
 
-    public static String SHA(String input) {
+    //SHAForRegister 还要加用户唯一标识盐 (这里用了邮箱)
+    public static String SHAForRegister(String input, String email) {
         try {
+            //加自定义盐
+            input = input+salt+email;
+
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
             byte[] hash = digest.digest(input.getBytes(StandardCharsets.UTF_8));
 
-            // 将字节数组转换为十六进制字符串
-            StringBuilder hexString = new StringBuilder();
-            for (byte b : hash) {
-                String hex = Integer.toHexString(0xff & b);
-                if (hex.length() == 1) {
-                    hexString.append('0');
-                }
-                hexString.append(hex);
-            }
-            return hexString.toString();
+            //转成base64字符串return
+            return Base64.getEncoder().encodeToString(hash);
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
@@ -34,13 +33,24 @@ public class EncryptUtil {
     }
 
     /**
-     * 鉴定哈希值是否相同
+     * 鉴定用户密码哈希值是否相同
      * @param inputPassword 输入的东西
      * @param real    正确的东西
      */
-    public static boolean verify(String inputPassword, String real) {
-        String hashedInput = SHA(inputPassword);
-        return real.equals(hashedInput);
+    public static boolean verifyUserPwd(String inputPassword, String email, String real) {
+        inputPassword = inputPassword+salt+email;
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            //利用加密算法哈希成对应字节数组
+            byte[] input = digest.digest(inputPassword.getBytes(StandardCharsets.UTF_8));
+            //将正确的Base64密码解成字节数组
+            byte[] realBytes = Base64.getDecoder().decode(real);
+            //比较两个字节数组
+            return MessageDigest.isEqual(input, realBytes);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 
 }
